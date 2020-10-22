ccdba2211ba9483354d8f260cd7d95e0f3281d8b
MSI\moneo <jvr.moneo@gmail.com>
2020-10-22 12:12:25 +0200

    Añadir @Input

--- src/app/shared/hijo/hijo.component.ts
+++ src/app/shared/hijo/hijo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hijo',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hijo.component.css']
 })
 export class HijoComponent implements OnInit {
+
+  //Definir el parámetro de entrada que me va a permitir recibir info desde el padre
+  @Input() parametroEntrada: string = "Sin nombre";
 
   constructor() { }
 
