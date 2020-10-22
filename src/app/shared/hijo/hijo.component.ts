c0d6ac11e86a54f1ae79cef7fdf8ddbf5dc79921
MSI\moneo <jvr.moneo@gmail.com>
2020-10-22 12:42:18 +0200

    Añadir output

--- src/app/shared/hijo/hijo.component.ts
+++ src/app/shared/hijo/hijo.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-hijo',
@@ -9,10 +9,20 @@ export class HijoComponent implements OnInit {
 
   //Definir el parámetro de entrada que me va a permitir recibir info desde el padre
   @Input() parametroEntrada: string = "Sin nombre";
+  //Defino el parametro de salida, en vez de string podemos pasar lo que quieras
+  @Output() parametroSalida = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  cambiarPropiedad(){
+    console.log('click en el button del hijo');
+    this.parametroEntrada = 'Ana Belén';
+    
+    //Aviso al padre, Ejecutando el evento de salida y pasandole el string, ejecuto el evento de salida pasándole
+    this.parametroSalida.emit(this.parametroEntrada);
   }
 
 }
